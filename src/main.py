b88d7b76ba5ce01c4e0930c63f8c8406c64a7787
André Jr Mello <andrejr.mello@hotmail.com>
2026-07-25 17:19:13 -0300

    código finalizado

--- src/main.py
+++ src/main.py
@@ -2,21 +2,29 @@ from machine import I2C, Pin
 from time import sleep, time
 import sys
 
+from libs.servo import Servo
 from libs.ssd1306 import SSD1306_I2C
 import libs.DFRobot_AHT20 as DFRobot_AHT20
 
 # Params
 TEMP_ON = 42.0
 TEMP_OFF = 47.0
+TEMP_MEDIO = 45.0
 
-HUM_ON = 25.0
-HUM_OFF = 20.0
+HUM_ON = 23
+HUM_OFF = 18
 
-EXAUSTOR_MIN_TIME = 30  # segundos
+EXAUSTOR_MAX_TIME = 45  # segundos
+
+SERVO_OPEN = 50
+SERVO_CLOSE = 148
 
 ptc_ligado = False
 exaustor_ligado = False
 exaustor_inicio = 0
+
+# Servo motor
+servo=Servo(pin=15)
 
 # Animação
 spinner_frames = ["|", "/", "-", "\\"]
@@ -33,11 +41,15 @@ def desligar_ptc():
 
 
 def ligar_exaustor():
+    servo.move(SERVO_OPEN) # abrir duto
+    sleep(1)
     exaustor.value(1)
 
 
 def desligar_exaustor():
     exaustor.value(0)
+    servo.move(SERVO_CLOSE) # fechar duto
+    sleep(1)
 
 
 i2c = I2C(0, sda=Pin(16), scl=Pin(17))
@@ -88,64 +100,93 @@ def read_AHT20():
         print("Erro leitura sensor:", e)
         raise e
 
+try:
+    servo.move(SERVO_CLOSE)  # iniciar fechado
+    inicio = time()
+    while True:
 
-while True:
+        temperatura, umidade = read_AHT20()
 
-    temperatura, umidade = read_AHT20()
+        if temperatura is None:
+            sleep(1)
+            continue
+    
+        if temperatura > 55:
+            print("Finalizando por segurança, temperatura acima de 55C")
+            desligar_ptc()
 
-    if temperatura is None:
-        sleep(3)
-        continue
+            if exaustor_ligado:
+                desligar_exaustor()
 
-    # --------------------
-    # CONTROLE DO PTC
-    # --------------------
+            break
 
-    if not ptc_ligado and temperatura <= TEMP_ON:
-        ligar_ptc()
-        ptc_ligado = True
-        print("PTC LIGADO")
+        # --------------------
+        # CONTROLE DO PTC
+        # --------------------
 
-    elif ptc_ligado and temperatura >= TEMP_OFF:
+        if not ptc_ligado and temperatura <= TEMP_ON:
+            ligar_ptc()
+            ptc_ligado = True
+            print("PTC LIGADO")
+
+        elif ptc_ligado and temperatura >= TEMP_OFF:
+            desligar_ptc()
+            ptc_ligado = False
+            print("PTC DESLIGADO")
+
+        # --------------------
+        # CONTROLE EXAUSTOR
+        # --------------------
+
+        agora = time()
+
+        if not exaustor_ligado and umidade >= HUM_ON and temperatura >= TEMP_MEDIO:
+            ligar_exaustor()
+            exaustor_ligado = True
+            exaustor_inicio = agora
+
+            print("EXAUSTOR LIGADO")
+
+        elif exaustor_ligado:
+
+            tempo_ligado = agora - exaustor_inicio
+
+            if umidade <= HUM_OFF or tempo_ligado >= EXAUSTOR_MAX_TIME:
+                desligar_exaustor()
+
+                exaustor_ligado = False
+
+                print("EXAUSTOR DESLIGADO")
+
+        # --------------------
+        # OLED
+        # --------------------
+
+        oled.text(f"PTC: {'ON' if ptc_ligado else 'OFF'}", 0, 25)
+        oled.text(f"Exaustor: {'ON' if exaustor_ligado else 'OFF'}", 0, 35)
+
+        # marcar tempo total de execução aplicação formato HH:MM:SS
+        tempo_total = time() - inicio
+        horas = int(tempo_total // 3600)
+        minutos = int((tempo_total % 3600) // 60)
+        segundos = int(tempo_total % 60)
+        oled.text(f"Tempo: {horas:02d}:{minutos:02d}:{segundos:02d}", 0, 45)
+
+        # Animação
+        oled.text(spinner_frames[spinner_idx], 120, 54)
+        spinner_idx = (spinner_idx + 1) % len(spinner_frames)
+
+        oled.show()
+
+        sleep(1)
+
+finally:
+    print("Desligando PTC e Exaustor...")
+    if ptc_ligado:
         desligar_ptc()
-        ptc_ligado = False
         print("PTC DESLIGADO")
-
-    # --------------------
-    # CONTROLE EXAUSTOR
-    # --------------------
-
-    agora = time()
-
-    if not exaustor_ligado and umidade >= HUM_ON and temperatura >= TEMP_ON:
-        ligar_exaustor()
-        exaustor_ligado = True
-        exaustor_inicio = agora
-
-        print("EXAUSTOR LIGADO")
-
-    elif exaustor_ligado:
-
-        tempo_ligado = agora - exaustor_inicio
-
-        if umidade <= HUM_OFF and tempo_ligado >= EXAUSTOR_MIN_TIME:
-            desligar_exaustor()
-
-            exaustor_ligado = False
-
-            print("EXAUSTOR DESLIGADO")
-
-    # --------------------
-    # OLED
-    # --------------------
-
-    oled.text(f"PTC: {'ON' if ptc_ligado else 'OFF'}", 0, 25)
-    oled.text(f"Exaustor: {'ON' if exaustor_ligado else 'OFF'}", 0, 35)
-
-    # Animação
-    oled.text(spinner_frames[spinner_idx], 120, 54)
-    spinner_idx = (spinner_idx + 1) % len(spinner_frames)
-
-    oled.show()
-
-    sleep(3)
+    if exaustor_ligado:
+        desligar_exaustor()
+        print("EXAUSTOR DESLIGADO")
+    servo.move(SERVO_CLOSE)
+    servo.stop()
